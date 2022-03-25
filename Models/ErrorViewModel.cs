e71cc4b15ffa5b0d3ea46fbea7a46b64a8c37a0a
Luis Felipe <luisfelipesdn12@gmail.com>
2022-03-25 20:26:00 -0300

    🎉 feat: Initialized template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspnet_movies.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
